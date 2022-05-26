8425dea9e6acfb9baf25f45712d1cd9cfe43fe07
Bao Mark Nguyen <bao.m.nguyen@hiof.no>
2022-05-26 17:08:51 +0200

    added shop preview, filtereing and only show the first 4

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import { Fragment, useContext } from "react";
 import { CategoriesContext } from "../../../contexts/categories.context";
-import ProductCard from "../../product-card/product-card.component";
+import CategoryPreview from "../../category-preview/category-preview.component";
 import "./shop.styles.scss"
 
 const Shop = () => {
@@ -11,20 +11,12 @@ const Shop = () => {
     return (
 
 
-        <Fragment>
-            {Object.keys(categoriesMap).map((title) => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className="products-container">
-                    {categoriesMap[title].map((product) => (
-                    
-                    <ProductCard key={product.id} product={product} />
-
-                    ))}
-                    </div>
-                </Fragment>
-            ))}
-       </Fragment>
+        <div className="shop-containter">
+            {Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title];
+                return <CategoryPreview key={title} title={title} products={products}/>
+            }   )}
+       </div>
     )
 }
 
